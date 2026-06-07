3b97966663535a111efa57bb65cdd4c9bbd2678e
Xlayync <igor.mejsner@cyfroweszkoly.pl>
2026-06-07 19:57:27 +0200

    add celestial objects dataset

---
+++ src/celestialObjects.tsx
@@ -0,0 +1,44 @@
+﻿const celestialObjects = [
+    {
+        id: 1,
+        name: "Mars",
+        type: "Planeta",
+        distance: "225 mln km",
+        image:
+            "https://images.unsplash.com/photo-1614728894747-a83421e2b9c9"
+    },
+    {
+        id: 2,
+        name: "Andromeda",
+        type: "Galaktyka",
+        distance: "2.5 mln lat świetlnych",
+        image:
+            "https://images.unsplash.com/photo-1462331940025-496dfbfc7564"
+    },
+    {
+        id: 3,
+        name: "Betelgeza",
+        type: "Gwiazda",
+        distance: "642 lata świetlne",
+        image:
+            "https://images.unsplash.com/photo-1502134249126-9f3755a50d78"
+    },
+    {
+        id: 4,
+        name: "Europa",
+        type: "Księżyc",
+        distance: "628 mln km",
+        image:
+            "https://images.unsplash.com/photo-1446776877081-d282a0f896e2"
+    },
+    {
+        id: 5,
+        name: "Messier 87",
+        type: "Galaktyka",
+        distance: "53 mln lat świetlnych",
+        image:
+            "https://images.unsplash.com/photo-1465101046530-73398c7f28ca"
+    }
+];
+
+export default celestialObjects;
